a94c03f2f26e1b08090b1bf2fe12c942c8ca6124
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2021-06-17 15:07:53 +0300

    Create orderPage.dart

---
+++ lib/pages/navBarPages/orderPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({Key? key}) : super(key: key);
+
+  @override
+  _OrderPageState createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
