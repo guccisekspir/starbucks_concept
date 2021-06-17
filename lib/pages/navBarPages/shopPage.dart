5d2bce1e611511e62b72900d84a19ac4002a25e6
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2021-06-17 15:07:57 +0300

    Create shopPage.dart

---
+++ lib/pages/navBarPages/shopPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({Key? key}) : super(key: key);
+
+  @override
+  _ShopPageState createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
