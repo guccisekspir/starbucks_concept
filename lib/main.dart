687e0cdce3a924aab7c37898b6b2cfef6006e8be
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2021-06-19 14:21:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
